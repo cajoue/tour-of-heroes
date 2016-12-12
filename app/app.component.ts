3671d663c96b6eb87f7b3b6e3b832c4b011a8e7f
cajoue <cajoue@users.noreply.github.com>
2016-12-12 15:50:41 +0100

    list the heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,32 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Ava' },
+  { id: 12, name: 'Rey' },
+  { id: 13, name: 'Oti Mabusi' },
+  { id: 14, name: 'Nadiya Hussein' },
+  { id: 15, name: 'Liz Lemon' },
+  { id: 16, name: 'Leslie Knope' },
+  { id: 17, name: 'Dawn French' },
+  { id: 18, name: 'Jennifer Saunders' },
+  { id: 19, name: 'Judi Dench' },
+  { id: 20, name: 'Kate McKinnon' }
+];
+
+// The (*) prefix to ngFor indicates that the <li> element and its children constitute a master template.
+// The ngFor directive iterates over the heroes array returned by the AppComponent.heroes property and stamps out instances of this template.
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -24,4 +46,5 @@ export class AppComponent  {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
 }
